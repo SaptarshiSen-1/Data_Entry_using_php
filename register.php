6c810aa2d7a747aad76a5289f46eb70d48a5021a
SaptarshiSen-1 <160160197+SaptarshiSen-1@users.noreply.github.com>
2026-04-07 10:20:09 +0530

    register.php

--- register.php
+++ register.php
@@ -4,10 +4,11 @@ include "db.php";
 if (isset($_POST['register'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
+    $phone = $_POST['phone'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO users (name, email, password) 
-            VALUES ('$name', '$email', '$password')";
+    $sql = "INSERT INTO users (name, email, phone, password) 
+            VALUES ('$name', '$email', '$phone', '$password')";
 
     if ($conn->query($sql)) {
         header("Location: login.php");
@@ -24,17 +25,18 @@ if (isset($_POST['register'])) {
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+<img class="bvec" src="Bvec_img.jpeg" alt="bvec">
 <div class="container">
     <h1 style="text-align:center">Register</h1>
     <form method="POST">
         <input type="text" name="name" placeholder="Name" required>
         <input type="email" name="email" placeholder="Email" required>
+        <input type="text" name="phone" placeholder="Phone Number" required>
         <input type="password" name="password" placeholder="Password" required>
         <button name="register">Register</button>
     </form>
     <br>
-    <a href="login.php">Login</a>
+    <a href="login.php" style="color:white">Login</a>
 </div>
 
 </body>
